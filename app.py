cd0444fa9fa5948e0a249bd86b4ff16f6e2f2423
Luis Fernando Castellanos Guarín <LuisFC@corteconstitucional.gov.co>
2025-05-22 15:55:43 -0500

    update key mongoDb
    update key mongoDb

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ CREATOR_APP = "Nombre del creador/ruta github"
 mongo_uri   = os.environ.get("MONGO_URI")
 
 if not mongo_uri:
-    uri = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?appName=Cluster0"
-    mongo_uri = uri
+    #uri = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?appName=Cluster0"
+    uri         = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    mongo_uri   = uri
 
 # Función para conectar a MongoDB
 def connect_mongo():
